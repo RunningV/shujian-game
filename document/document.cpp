11de394fef9ce11a94a66247803d965623668275
RunningV <1140353684@qq.com>
2017-04-16 21:40:02 +0800

    add document file :apple:

---
+++ document/document.cpp
@@ -0,0 +1,43 @@
+｛
+	"0;30m": "Black",
+	"0;31m": "Red",
+	"0;32m": "Green",
+	"0;33m": "Yellow",
+	"0;34m": "Blue",
+	"0;35m": "Magenta",
+	"0;36m": "Cyan",
+	"0;37m": "White",
+
+			/*  Hi Intensity Foreground Colors  */
+
+	"1;31m": "Red",
+	"1;32m": "Green",
+	"1;33m": "Yellow",
+	"1;34m": "Blue",
+	"1;35m": "Magenta",
+	"1;36m": "Cyan",
+	"1;37m": "White",
+
+			/*  Hi Intensity Background Colors  */
+
+	"41;1m":" Red",
+	"42;1m":" Green",
+	"43;1m":" Yellow",
+	"44;1m":" Blue",
+	"45;1m":" Magenta",
+	"46;1m":" Cyan",
+	"47;1m":" White",
+
+			/*  Background Colors  */
+
+	"40m": "Black",
+	"41m": "Red",
+	"42m": "Green",
+	"43m": "Yellow",
+	"44m": "Blue",
+	"45m": "Magenta",
+	"46m": "Cyan",
+	"47m": "White",
+
+	"0;0m": "orange"
+}
