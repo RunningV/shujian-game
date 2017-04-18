3152d739bf4db5ba5fa2f21d39dff2eb58163f4a
擂鼓人running <1140353684@qq.com>
2017-04-18 16:18:28 +0800

    :sun:

--- app.js
+++ app.js
@@ -14,60 +14,70 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var server = new net.Socket({
-  allowHalfOpen: true,
-  readable: true,
-  writable: true
-});
-
-var loginData = null;
+var server;
 
 io.on('connection', function(socket) {
+  var loginData;
+
+  var server = new net.Socket({
+    allowHalfOpen: true,
+    readable: true,
+    writable: true
+  });
+  
 	socket.on('login', function(data) {
-		console.log(data);
 		loginData = data;
+
 		server.connect(5555, '123.206.188.205', function() {
+      server.setKeepAlive(true);
 		  console.log('connect success !')
 		  server.write('n\n');
 		})
 	});
+
   socket.on('order', function(data) {
     server.write(data);
     server.write('\n');
   });
+
   socket.on('logout', function() {
     server.write('quit');
     server.write('\n');
     server.destroy();
   })
+
+  server.on('data', function(buffer) {
+    var buf = convert(buffer, 'utf8', 'gb2312');
+    var buf2Str = buf.toString()//.replace(/(\s+|\n)/gm, 'BLANK');
+    console.log(buf2Str)
+    if(buf2Str.match('英文名字')) {
+      console.log('输入英文ID')
+      server.write(loginData.id)
+      server.write('\n')
+    } else if (buf2Str.match('识别密码')) {
+      console.log('密码(passwd)')
+      server.write(loginData.pwd)
+      server.write('\n')
+      io.emit('success', ' mock login success')
+    } else {
+      io.emit('message', buf2Str);
+    }
+  })
+
+  server.on('end', function(data) {
+    console.log(data, 11)
+    console.log(server, 13)
+  })
+
+  /*server.on('close', function(data) {
+    console.log(data, 12)
+  })*/
+
+  server.on('error', err => {
+    console.log(err,33);
+  })
 })
 
-server.on('data', function(buffer) {
-	var buf = convert(buffer, 'utf8', 'gb2312');
-	var buf2Str = buf.toString()//.replace(/(\s+|\n)/gm, 'BLANK');
-  console.log(buf2Str)
-	if(buf2Str.match('英文名字')) {
-		console.log('输入英文ID')
-		server.write(loginData.id)
-		server.write('\n')
-	} else if (buf2Str.match('识别密码')) {
-    console.log('密码(passwd)')
-    server.write(loginData.pwd)
-    server.write('\n')
-    io.emit('success', ' mock login success')
-	} else {
-    io.emit('message', buf2Str);
-  }
-
-})
-
-server.on('end', function(data) {
-  console.log(data, 11)
-  console.log(server, 13)
-})
-server.on('close', function(data) {
-  console.log(data, 12)
-})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
@@ -87,7 +97,6 @@ require('./routes/router')(app, express, path);
 
 http.listen(port);
 http.on('error', onError);
-http.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -135,16 +144,4 @@ function onError(error) {
     default:
       throw error;
   }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
 }
